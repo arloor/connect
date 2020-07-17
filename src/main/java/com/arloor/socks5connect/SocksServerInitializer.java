b1491ad297772645f8749932247347df1e1904c0
liuganghuan <liuganghuan@meituan.com>
2020-07-17 18:02:32 +0800

    Revert "accept异常日志"
    This reverts commit bb91e247

--- src/main/java/com/arloor/socks5connect/SocksServerInitializer.java
+++ src/main/java/com/arloor/socks5connect/SocksServerInitializer.java
@@ -1,20 +1,15 @@
 
 package com.arloor.socks5connect;
 
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.socksx.SocksPortUnificationServerHandler;
 import io.netty.handler.traffic.ChannelTrafficShapingHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.arloor.socks5connect.ClientBootStrap.SpeedLimitKB;
 
 
 public final class SocksServerInitializer extends ChannelInitializer<SocketChannel> {
-    private static Logger logger = LoggerFactory.getLogger(RelayHandler.class.getSimpleName());
-
     @Override
     public void initChannel(SocketChannel ch) throws Exception {
         if(SpeedLimitKB>0){
@@ -23,9 +18,5 @@ public final class SocksServerInitializer extends ChannelInitializer<SocketChann
         ch.pipeline().addLast(
                 new SocksPortUnificationServerHandler(),
                 SocksServerHandler.INSTANCE);
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
     }
 }
