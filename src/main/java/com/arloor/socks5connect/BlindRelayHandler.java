f6d2071b067603e7cdbe15a1300c34ec999e5b67
liuganghuan <liuganghuan@meituan.com>
2020-08-04 19:32:54 +0800

    可以

--- src/main/java/com/arloor/socks5connect/BlindRelayHandler.java
+++ src/main/java/com/arloor/socks5connect/BlindRelayHandler.java
@@ -5,6 +5,10 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
 import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,8 +38,22 @@ public final class BlindRelayHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
         if (relayChannel.isActive()) {
-            relayChannel.writeAndFlush(msg);
+            HttpRequest request =null;
+            if(msg instanceof HttpRequest){
+                request = (HttpRequest) msg;
+                if(request.getMethod().equals(HttpMethod.CONNECT)){
+                    ctx.pipeline().remove(HttpRequestDecoder.class);
+                }
+                System.out.println(request);
+            }
+            HttpRequest finalRequest = request;
+            relayChannel.writeAndFlush(msg).addListener(future -> {
+                if (future.isSuccess()&& finalRequest !=null&& finalRequest.getMethod().equals(HttpMethod.CONNECT)){
+                    relayChannel.pipeline().remove(HttpRequestEncoder.class);
+                }
+            });
         } else {
             ReferenceCountUtil.release(msg);
         }
