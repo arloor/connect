b120272dfc658dd7e5e6afaa8591ca7418965581
arloor <liuganghuan@meituan.com>
2021-11-05 15:01:58 +0800

    reactor模式实现

--- src/main/java/com/arloor/connect/Connector.java
+++ src/main/java/com/arloor/connect/Connector.java
@@ -9,27 +9,33 @@ import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Connector implements Runnable {
     private Selector selector;
-    private ConcurrentLinkedQueue<SocketChannel> waitQueue = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<SocketWrapper> waitQueue = new ConcurrentLinkedQueue<>();
+    private ThreadPoolExecutor pool = new ThreadPoolExecutor(4, 4, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10000));
 
     public Connector() throws IOException {
         selector = Selector.open();
     }
 
-    public SocketChannel connect(String host, int port) {
+    public SocketWrapper connect(String host, int port) {
         try {
             final SocketChannel socketChannel = SocketChannel.open();
             // 先阻塞connect
             socketChannel.socket().connect(new InetSocketAddress(host, port), 5000);
             // 设置为非阻塞
             socketChannel.configureBlocking(false);
-            waitQueue.add(socketChannel);
+            final SocketWrapper socketWrapper = new SocketWrapper(socketChannel);
+            waitQueue.add(socketWrapper);
             selector.wakeup();
-            return socketChannel;
+            return socketWrapper;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,9 +46,9 @@ public class Connector implements Runnable {
     public void run() {
         try {
             while (true) {
-                SocketChannel socketChannel;
-                while ((socketChannel = waitQueue.poll()) != null) {
-                    socketChannel.register(selector, SelectionKey.OP_READ, new Object());
+                SocketWrapper socketWrapper;
+                while ((socketWrapper = waitQueue.poll()) != null) {
+                    socketWrapper.socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, socketWrapper);
                 }
                 final int numKey = selector.select(1);
                 if (numKey > 0) {
@@ -50,11 +56,10 @@ public class Connector implements Runnable {
                     Iterator<SelectionKey> keyIterator = keys.iterator();
                     while (keyIterator.hasNext()) {
                         SelectionKey key = keyIterator.next();
-                        if (key.isReadable()) {//读来自远程服务器的响应
-                            String content = readAll(key);
-                            System.out.println(content);
-                        }
                         keyIterator.remove();
+                        // 取消注册，防止其他线程处理该key
+                        key.interestOps(key.interestOps() & (~key.readyOps()));
+                        pool.execute(new Processor(key));
                     }
                 }
             }
@@ -64,46 +69,19 @@ public class Connector implements Runnable {
 
     }
 
-    private String readAll(SelectionKey key) {
-        final SocketChannel channel = (SocketChannel) key.channel();
-        int cap = 1024;
-        ByteBuffer buffer = ByteBuffer.allocate(cap);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try {
-            int read;
-            while ((read = channel.read(buffer)) > 0) {
-                buffer.flip();
-                final byte[] dst = new byte[read];
-                buffer.get(dst);
-                out.write(dst);
-                buffer.clear();
-            }
-            if (read == -1) {
-                key.cancel();
-                channel.close();
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-            key.cancel();
-            try {
-                channel.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return out.toString();
-    }
-
     public static void main(String[] args) throws Exception {
         Connector connector = new Connector();
         Thread thread = new Thread(connector, "connector");
         thread.setDaemon(true);
         thread.start();
-        final SocketChannel socketChannel = connector.connect("sg.gcall.me", 80);
+        final SocketWrapper socketWrapper = connector.connect("sg.gcall.me", 80);
+        socketWrapper.writeAll("GET / HTTP/1.1\r\nHost: sg.gcall.me\r\n\r\n");
+        Thread.sleep(10);
         while (true) {
             try {
-                if (socketChannel.isOpen()) {
-                    writeAll(socketChannel, "GET / HTTP/1.1\r\nHost: sg.gcall.me\r\n\r\n");
+                if (socketWrapper.socketChannel.isOpen()) {
+                    socketWrapper.writeAll("GET / HTTP/1.1\r\nHost: sg.gcall.me\r\n\r\n");
+                    Thread.sleep(10);
                 } else {
                     System.out.println("closed");
                     break;
@@ -112,13 +90,118 @@ public class Connector implements Runnable {
                 e.printStackTrace();
             }
         }
-        Thread.sleep(100);
+        Thread.sleep(10000);
     }
 
-    private static void writeAll(SocketChannel socketChannel, String msg) throws IOException {
-        ByteBuffer src = ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
-        while (src.hasRemaining()) {
-            socketChannel.write(src);
+
+    public static class SocketWrapper {
+        private Queue<ByteBuffer> writeBuffers = new ArrayBlockingQueue<>(100);
+        private ByteArrayOutputStream readBuffer = new ByteArrayOutputStream();
+        private SocketChannel socketChannel;
+
+        public SocketWrapper(SocketChannel socketChannel) {
+            this.socketChannel = socketChannel;
+        }
+
+        public void writeAll(String msg) {
+            ByteBuffer src = ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
+            this.writeBuffers.offer(src);
+        }
+
+        private void readAll() throws NetException {
+            final SocketChannel channel = this.socketChannel;
+            int cap = 1024;
+            ByteBuffer buffer = ByteBuffer.allocate(cap);
+            ByteArrayOutputStream out = this.readBuffer;
+            int read;
+            try {
+                while ((read = channel.read(buffer)) > 0) {
+                    buffer.flip();
+                    final byte[] dst = new byte[read];
+                    buffer.get(dst);
+                    out.write(dst);
+                    buffer.clear();
+                }
+            } catch (Throwable e) {
+                throw new NetException("Unknow", e);
+            }
+            if (read == -1) {
+                throw new NetException("EOF");
+            }
+        }
+
+        public void flush() {
+            ByteBuffer buffer;
+            while ((buffer = writeBuffers.peek()) != null) {
+                try {
+                    final int write = socketChannel.write(buffer);
+                    System.out.println(Thread.currentThread() + " write " + write);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (!buffer.hasRemaining()) {
+                    writeBuffers.poll();
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static final class Processor implements Runnable {
+        private SelectionKey key;
+
+        public Processor(SelectionKey key) {
+            this.key = key;
+        }
+
+        @Override
+        public void run() {
+            if (key.isValid()) {
+                final SocketWrapper socketWrapper = (SocketWrapper) key.attachment();
+                if (socketWrapper != null) {
+                    try {
+                        if (key.isReadable()) {
+                            socketWrapper.readAll();
+                            System.out.println(Thread.currentThread() + " read字节数 " + socketWrapper.readBuffer.size());
+                        }
+                        if (key.isWritable()) {
+                            socketWrapper.flush();
+                        }
+                    } catch (NetException e) {
+                        key.cancel();
+                        try {
+                            socketWrapper.socketChannel.close();
+                        } catch (IOException ex) {
+                            System.err.println("close 失败");
+                        }
+                        return;
+                    }
+                }
+                // 处理完毕，再次注册事件
+                key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+            }
+        }
+    }
+
+    public static final class NetException extends Exception {
+        public NetException() {
+        }
+
+        public NetException(String message) {
+            super(message);
+        }
+
+        public NetException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public NetException(Throwable cause) {
+            super(cause);
+        }
+
+        public NetException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+            super(message, cause, enableSuppression, writableStackTrace);
         }
     }
 }
